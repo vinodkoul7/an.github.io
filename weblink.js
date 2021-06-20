15bf92a55cb6212bd276449c3238c3afb730370f
AN IT Support Specialist 09 <an_itsupportspecialist09@rccl.com>
2021-06-20 22:54:47 +0300

    gg

--- weblink.js
+++
@@ -1,7 +0,0 @@
-var links = document.getElementById('weblist');
-links.onchange = function () {
-    var userlink = this.options [this.selectedIndex];
-    if (userOption.value != "nothing") {
-        window.open(userOption.value, "")
-    }
-}
